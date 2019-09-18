bdf8035c82d6afe8dc8767a4b4271793dccff811
dbaldazzi <d.baldazzi@hotmail.com>
2019-09-18 13:10:27 -0600

    create tasks button added to lists

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -3,18 +3,22 @@
     <div class="lists">
       <div class="row">
         <div class="col-3 border m-5 d-flex justify-content-around">
-          <h3>
             {{listProp.title}}
             <button class="btn btn-danger" @click="deleteList">x</button>
-          </h3>
+          <button type="button" data-toggle="modal" data-target="#create-task-modal">Create Task</button>
         </div>
       </div>
     </div>
+    <Task v-for="task in tasks" :taskProp="task" :key="task._id" />
+ <CreateTaskModal />
   </div>
 </template>
 
 
 <script>
+import CreateTaskModal from "../components/CreateTaskModal.vue";
+import Tasks from "../components/Tasks.vue";
+
 export default {
   name: "lists",
   props: ["listProp"],
