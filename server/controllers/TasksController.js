17208122082c5ed651fc4dc7dcecb5d40fdb2d99
Nick Minor <nickminor1991@gmail.com>
2019-09-18 12:07:26 -0600

    TaskService and Controller created

---
+++ server/controllers/TasksController.js
@@ -0,0 +1,73 @@
+import TaskService from "../services/TaskService"
+import express from 'express'
+import { Authorize } from "../middleware/authorize.js"
+
+let _ts = new TaskService().repository
+
+export default class TaskController {
+
+    constructor() {
+        this.router = express.Router()
+            .use(Authorize.authenticated)
+            .get('', this.getAll)
+            .post('', this.createTask)
+            .put('/:id', this.edit)
+            .delete('/:id', this.deleteTask)
+    }
+    async getAll(req, res, next) {
+        try {
+            let data = await _ts.find({})
+            return res.send(data)
+        } catch (error) { next(error) }
+    }
+
+    async createTask(req, res, next) {
+        try {
+            req.body.authorId = req.session.uid
+            let data = await _ts.create(req.body)
+            res.send(data)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async edit(req, res, next) {
+        try {
+            let data = await _ts.findOneAndUpdate({ _id: req.params.id, user: req.session.uid }, req.body, { new: true })
+            if (data) {
+                return res.send(data)
+            }
+            throw new Error("Invalid Id")
+        } catch (error) { next(error) }
+    }
+    async deleteTask(req, res, next) {
+        try {
+            let data = await _ts.findOneAndRemove({ _id: req.params.id, authorId: req.session.uid })
+            if (!data) {
+                throw new Error('Invalid Id')
+            }
+            res.send('task deleted')
+        } catch (error) { next(error) }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
