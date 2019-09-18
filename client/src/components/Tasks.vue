866febcba4a0965c74c427c7e5d4a15b2560ec0c
dbaldazzi <d.baldazzi@hotmail.com>
2019-09-18 12:46:07 -0600

    created task vue

--- client/src/components/Tasks.vue
+++ client/src/components/Tasks.vue
@@ -1,16 +1,41 @@
 <template>
-  <div class="component"></div>
+  <div class="container-fluid">
+    <div class="tasks">
+      <div class="row"> 
+        <div class="col-3 border m-5"> 
+          <h5>
+            {{taskProp.description}}
+            <button class="btn btn-danger" @click="deleteTask">x</button>
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name: "component",
+  name: "tasks",
+  props: ["taskProp"],
   data() {
-    return {};
+    return {
+      newTask: {
+        description: ""
+      }
+    };
   },
-  computed: {},
-  methods: {},
+  mounted() {},
+  computed: {
+    postTask() {
+      return this.$store.state.tasks;
+    }
+  },
+  methods: {
+    deleteTask() {
+      this.$store.dispatch("deleteTask", this.taskProp);
+    }
+  },
   components: {}
 };
 </script>
